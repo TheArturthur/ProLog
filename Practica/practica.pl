5486640c13ad879844824ffc30cbafe3c0829435
TheArturthur <arturo.vidal.96@gmail.com>
2019-04-03 19:43:36 +0200

    coloresIncluidos empezado. Falla listaColores.

--- Practica/practica.pl
+++ Practica/practica.pl
@@ -9,7 +9,7 @@ nat(0).
 %Si X es natural, X+1 tambien.
 nat(s(X)) :- nat(X).
 
-menor_igual(s(0),X) :- X\=0.
+menor_igual(0,X).
 menor_igual(s(X),s(Y)) :- menor_igual(X,Y).
 
 mayor_igual(s(X),0) :- nat(X).
@@ -23,6 +23,12 @@ resta(s(X),s(Y),Z) :- resta(X,Y,Z).
 
 iguales(s(0),s(0)).
 iguales(s(A),s(B)) :- iguales(A,B).
+
+par(0).
+par(s(s(X))) :- par(X).
+
+impar(s(0)).
+impar(s(s(X))) :- impar(X).
 
 esPieza(Anchura,Altura,Profundidad,Color) :-
 	nat(Anchura),
@@ -47,11 +53,13 @@ esTorre([pieza(Anchura1,Altura1,Profundidad1,Color1),pieza(Anchura2,Altura2,Prof
 
 alturaTorre([pieza(Anchura,Altura,Profundidad,Color)|[]],X) :-
 	esTorre([pieza(Anchura,Altura,Profundidad,Color)|[]]),
+	nat(X),
 	resta(X,Altura,Y),
 	iguales(s(Y),s(0)).
 
 alturaTorre([pieza(Anchura1,Altura1,Profundidad1,Color1),pieza(Anchura2,Altura2,Profundidad2,Color2)|Ps],X) :-
 	esTorre([pieza(Anchura1,Altura1,Profundidad1,Color1),pieza(Anchura2,Altura2,Profundidad2,Color2)|Ps]),
+	nat(X),
 	resta(X,Altura1,Y),
 	alturaTorre([pieza(Anchura2,Altura2,Profundidad2,Color2)|Ps],Y).
 
@@ -64,9 +72,40 @@ coloresTorre([pieza(Anchura1,Altura1,Profundidad1,Color1),pieza(Anchura2,Altura2
 	member(Color1,[Colores1,Colores2|Cs]),
 	coloresTorre([pieza(Anchura2,Altura2,Profundidad2,Color2)|Ps],[Colores2|Cs]).
 
-%coloresIncluidos() :-.
+coloresIncluidos([pieza(Anchura1,Altura1,Profundidad1,Color1)|[]],[pieza(Anchura2,Altura2,Profundidad2,Color2)|[]]) :-
+	esTorre([pieza(Anchura1,Altura1,Profundidad1,Color1)|[]]),
+	esTorre([pieza(Anchura2,Altura2,Profundidad2,Color2)|[]]),
+	Color1 = Color2.
 
-%coloresIncluidos() :-	.
+coloresIncluidos([pieza(Anchura1_1,Altura1_1,Profundidad1_1,Color1_1),pieza(Anchura1_2,Altura1_2,Profundidad1_2,Color1_2)|Ps1],
+	[pieza(Anchura2_1,Altura2_1,Profundidad2_1,Color2_1),pieza(Anchura2_2,Altura2_2,Profundidad2_2,Color2_2)|Ps2]) :-
+ esTorre([pieza(Anchura1_1,Altura1_1,Profundidad1_1,Color1_1),pieza(Anchura1_2,Altura1_2,Profundidad1_2,Color1_2)|Ps1]),
+ esTorre([pieza(Anchura2_1,Altura2_1,Profundidad2_1,Color2_1),pieza(Anchura2_2,Altura2_2,Profundidad2_2,Color2_2)|Ps2]),
+ listaColores([pieza(_,_,_,Color2_1),pieza(_,_,_,Color2_2)|Ps2],[Colores|Cs]),
+ estaColor([pieza(_,_,_,Color1_1),pieza(_,_,_,Color1_2)|Ps1],[Colores|Cs]).
 
-%esEdificioPar() :-	.
+listaColores([pieza(_,_,_,Color)|[]],[Colores|[]]) :-
+	p([Colores|[]],Color,_).
+
+listaColores([pieza(_,_,_,Color)|[]],[Colores|Cs]) :-
+	p([Colores|Cs],_,Color).
+
+listaColores([pieza(_,_,_,Color1),pieza(_,_,_,Color2)|Ps],[Colores|Cs]) :-
+	p([Colores|Cs],Color1,_),
+	listaColores([pieza(_,_,_,Color2)|Ps],[Colores|Cs]).
+
+listaColores([pieza(_,_,_,Color1),pieza(_,_,_,Color2)|Ps],[Colores|Cs]) :-
+	p([Colores|Cs],_,Color1),
+	listaColores([pieza(_,_,_,Color2)|Ps],[Colores,Cs|NCs].
+
+estaColor([pieza(_,_,_,Color)|[]],[Colores|Cs]) :-
+	member(Color,[Colores|Cs]).
+
+estaColor([pieza(_,_,_,Color1),pieza(_,_,_,Color2)|Ps],[Colores|Cs]) :-
+	member(Color1,[Colores|Cs]),
+	estaColor([pieza(_,_,_,Color2)|Ps],[Colores|Cs]).
+
+%esEdificioPar() :- .
+
+%esEdificioPiramide() :- .
 
